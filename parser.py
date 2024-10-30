bbba3eac3a3bc80eebf07e0e80dde02fd1ae86cb
VTiganov <vadimyxy@gmail.com>
2024-10-30 19:57:13 +0300

    modified:   parser.py

--- parser.py
+++ parser.py
@@ -3,6 +3,7 @@ import os
 from dotenv import load_dotenv
 from telethon import TelegramClient
 from telethon.tl.types import PeerChannel, PeerChat, PeerUser, Channel, Chat, User
+import asyncio
 
 # Load environment variables from .env file
 load_dotenv()
@@ -45,12 +46,29 @@ async def save_chats_to_csv(filename):
             else:
                 print(f"Skipped dialog {dialog.title} - Unsupported type: {type(dialog.entity)}")
 
-def export_chats(filename):
+async def export_chats(filename):
     """Wrapper to start the client and call the async function."""
-    async def main():
-        await client.start()
-        await save_chats_to_csv(filename)
-        print(f"All chats saved to {filename}")
+    await client.start()
+    await save_chats_to_csv(filename)
+    print(f"All chats saved to {filename}")
     
-    with client:
-        client.loop.run_until_complete(main())
+def main():
+    """Choose options or exit"""
+    while True:
+        print("\nChoose an option:")
+        print("1. Export chats to CSV")
+        print("0. Exit")
+        
+        choice = input("Enter your choice: ")
+        
+        if choice == "1":
+            filename = input("Enter the filename to save chats (e.g., chats.csv): ")
+            if not filename.lower().endswith('.csv'):
+                filename += '.csv'
+            
+            asyncio.run(export_chats(filename))
+        elif choice == "0":
+            print("Exiting program.")
+            break
+        else:
+            print("Invalid choice. Please enter 0 or 1.")
